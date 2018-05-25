51e19ab91e95616777cb9c4e84f40d24440cd74a
lly-uwt <lanly@uw.edu>
2018-05-25 15:40:52 -0700

    lambda update

--- lambda-eclipse/src/LambdaFunctionHandler.java
+++ lambda-eclipse/src/LambdaFunctionHandler.java
@@ -2,15 +2,16 @@
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.commons.math3.util.CombinatoricsUtils;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 
     @Override
     public String handleRequest(Object input, Context context) {
         context.getLogger().log("Input: " + input);
-
-        // TODO: implement your handler
-        return "Hello from Lambda!";
+        long timestamp = System.currentTimeMillis();
+        long result = CombinatoricsUtils.factorial(5);
+        timestamp = System.currentTimeMillis() - timestamp;
+        return result + "," + timestamp;
     }
-
 }
