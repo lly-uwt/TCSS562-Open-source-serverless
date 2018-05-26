60a030c321f4fc1cb634dc65e9c5ade746fa1f2b
lly-uwt <lanly@uw.edu>
2018-05-25 18:18:57 -0700

    Update FnFunction

--- fn-java-function/src/main/java/HelloFunction.java
+++
@@ -1,9 +0,0 @@
-public class HelloFunction {
-
-    public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
-
-        return "Hello, " + name + "!";
-    }
-
-}
