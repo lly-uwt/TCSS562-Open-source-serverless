a96bae8b0b162611a4d8e4d691733b81ba2c8e91
lly-uwt <lanly@uw.edu>
2018-06-08 06:57:06 -0700

    update code

--- fn-java-function/src/main/java/FnFunction.java
+++ fn-java-function/src/main/java/FnFunction.java
@@ -1,12 +1,21 @@
+import java.util.Random;
+
 import org.apache.commons.math3.util.CombinatoricsUtils;
 
 public class FnFunction {
+    public static int MAX = 10000;
 
     public String handleRequest(String input) {
+        Random rn = new Random();
         // String name = (input == null || input.isEmpty()) ? "world"  : input;
         long timestamp = System.currentTimeMillis();
         long result = CombinatoricsUtils.factorial(5);
-        timestamp = System.currentTimeMillis() - timestamp;
-        return result + "," + timestamp;
+        for (int i = 0; i < MAX; i++) {
+			for (int j = 0; j < MAX; j++) {
+				CombinatoricsUtils.factorial(rn.nextInt(10) + 1);
+			}
+		}
+		timestamp = System.currentTimeMillis() - timestamp;
+		return "" + timestamp;
     }
 }
--- lambda-eclipse/src/main/java/LambdaFunctionHandler.java
+++ lambda-eclipse/src/main/java/LambdaFunctionHandler.java
@@ -1,17 +1,26 @@
 
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Random;
+
 import org.apache.commons.math3.util.CombinatoricsUtils;
 
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+	public static int MAX = 10000;
 
-    @Override
-    public String handleRequest(Object input, Context context) {
-        // context.getLogger().log("Input: " + input);
-        long timestamp = System.currentTimeMillis();
-        long result = CombinatoricsUtils.factorial(5);
-        timestamp = System.currentTimeMillis() - timestamp;
-        return result + "," + timestamp;
-    }
+	@Override
+	public String handleRequest(Object input, Context context) {
+		Random rn = new Random();
+		// context.getLogger().log("Input: " + input);
+		long timestamp = System.currentTimeMillis();
+
+		for (int i = 0; i < MAX; i++) {
+			for (int j = 0; j < MAX; j++) {
+				CombinatoricsUtils.factorial(rn.nextInt(10) + 1);
+			}
+		}
+		timestamp = System.currentTimeMillis() - timestamp;
+		return "" + timestamp;
+	}
 }
